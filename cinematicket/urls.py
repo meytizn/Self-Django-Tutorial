d18d8df232bfbac0e448ce4cb2569b4a2791d18c
meytizn <zamanianmehdi23desktop@gmail.com>
2024-08-07 12:43:05 +0330

    static and img recognized

--- cinematicket/urls.py
+++ cinematicket/urls.py
@@ -4,6 +4,7 @@
 from django.urls import path,include
 from . import views
 
+
 app_name="cinematicket"
 
 
@@ -11,7 +12,9 @@ app_name="cinematicket"
 
 urlpatterns = [
     
-    path('',views.Home.as_view(),name='home'),
+    path('home',views.Home.as_view(),name='home'),
     path('product/',views.Product.as_view(),name='product'),
-    path('food/',views.Conssention.as_view(),name='food')
+    path('',views.Conssention.as_view(),name='food')
+    
+  
 ]   
