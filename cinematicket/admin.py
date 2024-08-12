e2e4273f437bfdab1a0bd02b349b6af246e7d2c0
meytizn <zamanianmehdi23desktop@gmail.com>
2024-08-12 14:20:19 +0330

    Category model created and coneccted with blog

--- cinematicket/admin.py
+++ cinematicket/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
 
-from .models import Food
+from .models import Food,Category
+
+
 
 admin.site.register(Food)
+
+admin.site.register(Category)
+
 
 # Register your models here.
--- cinematicket/models.py
+++ cinematicket/models.py
@@ -12,6 +12,7 @@ class Food(models.Model):
   rate=models.IntegerField(_("امتیاز محصول"))
   time=models.IntegerField(_("زمان انتظار"))
   price=models.IntegerField(_("قیمت محصول"))
+  category=models.ForeignKey("Category", verbose_name=_("دسته بندی محصول  "), on_delete=models.CASCADE,blank=True,null=True)
   pub_date=models.DateField(_("تاریخ انتشار"), auto_now=False, auto_now_add=True)
 
   def __str__(self):
@@ -19,3 +20,12 @@ class Food(models.Model):
   
   def get_absolute_url(self):
     return reverse ("cinematicket:food_list")
+
+
+class Category(models.Model):
+  title=models.CharField(_("عنوان دسته بندی "), max_length=50)
+  slug=models.SlugField(_("عنوان دسته بندی لاتین "))
+  pub_date=models.DateField(_("زمان انتشار"), auto_now=False, auto_now_add=True)
+
+  def __str__(self):
+    return self.title
